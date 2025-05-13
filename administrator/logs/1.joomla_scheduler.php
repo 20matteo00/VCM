fedd487f09dfc6f82743a30f10e2bc794eff35f9
Matteo Moscone <matteo.moscone.1atgiorgi@gmail.com>
2025-05-13 16:18:21 +0200

    admin

---
+++ administrator/logs/1.joomla_scheduler.php
@@ -0,0 +1,48 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-04-02 18:40:27 UTC
+#Software: Joomla! 5.2.5 Stable [ Uthabiti ] 11-March-2025 16:00 GMT
+
+#Fields: date	time	priority	message
+2025-04-02	18:40:27	INFO	Attività terminata con successo#01 in 0.00 (rete 0.00) secondi.
+2025-04-09	07:43:34	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-09	07:43:34	INFO	Attività> SessionGC end
+2025-04-09	07:43:34	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-13	11:22:32	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-13	11:22:32	INFO	Attività terminata con successo#03 in 0.26 (rete 0.26) secondi.
+2025-04-13	11:22:37	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-13	11:22:37	INFO	Attività> SessionGC end
+2025-04-13	11:22:37	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-15	07:34:39	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-15	07:34:39	INFO	Attività terminata con successo#03 in 0.24 (rete 0.24) secondi.
+2025-04-18	10:54:41	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-18	10:54:41	INFO	Attività> SessionGC end
+2025-04-18	10:54:41	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-18	10:54:45	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-18	10:54:45	INFO	Attività terminata con successo#03 in 0.26 (rete 0.26) secondi.
+2025-04-20	09:25:39	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-20	09:25:39	INFO	Attività> SessionGC end
+2025-04-20	09:25:39	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-20	09:25:43	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-20	09:25:43	INFO	Attività terminata con successo#03 in 0.22 (rete 0.22) secondi.
+2025-04-21	17:21:46	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-21	17:21:46	INFO	Attività> SessionGC end
+2025-04-21	17:21:46	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-25	00:44:24	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-25	00:44:24	INFO	Attività terminata con successo#03 in 0.26 (rete 0.26) secondi.
+2025-04-28	02:16:27	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-28	02:16:27	INFO	Attività> SessionGC end
+2025-04-28	02:16:27	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-04-29	11:23:21	INFO	Esecuzione attività#03 'Update Notification'.
+2025-04-29	11:23:22	INFO	Attività> UpdateNotification end
+2025-04-29	11:23:22	INFO	Attività terminata con successo#03 in 0.98 (rete 0.98) secondi.
+2025-04-29	11:23:24	INFO	Esecuzione attività#02 'Session GC'.
+2025-04-29	11:23:24	INFO	Attività> SessionGC end
+2025-04-29	11:23:24	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-05-02	12:59:57	INFO	Esecuzione attività#03 'Update Notification'.
+2025-05-02	12:59:58	INFO	Attività> UpdateNotification end
+2025-05-02	12:59:58	INFO	Attività terminata con successo#03 in 0.81 (rete 0.81) secondi.
+2025-05-04	13:03:59	INFO	Esecuzione attività#02 'Session GC'.
+2025-05-04	13:03:59	INFO	Attività> SessionGC end
+2025-05-04	13:03:59	INFO	Attività terminata con successo#02 in 0.00 (rete 0.00) secondi.
+2025-05-06	12:38:32	INFO	Esecuzione attività#01 'Rotate Logs'.
---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-05-13 13:16:40 UTC
+#Software: Joomla! 5.2.5 Stable [ Uthabiti ] 11-March-2025 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2025-05-13T13:16:40+00:00	INFO 151.83.132.130	joomlafailure	Il nome utente e la password non corrispondono o non hai ancora un account.
+2025-05-13T13:16:46+00:00	INFO 151.83.132.130	joomlafailure	Il nome utente e la password non corrispondono o non hai ancora un account.
+2025-05-13T13:16:50+00:00	INFO 151.83.132.130	joomlafailure	Il nome utente e la password non corrispondono o non hai ancora un account.
